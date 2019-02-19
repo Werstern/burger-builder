b6e09a14b855a49df5cae94ce2c599688279f965
Werstern <werstern@bigmir.net>
2019-02-19 22:44:19 +0200

    Fixing a common validation gotcha

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -108,18 +108,18 @@ class contactData extends Component {
   }
 
   checkValidaty(value, rules) {
-    let isValid = false;
+    let isValid = true;
 
     if (rules.required) {
-      isValid = value.trim() !== '';
+      isValid = value.trim() !== '' && isValid;
     }
 
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength
+      isValid = value.length >= rules.minLength && isValid
     }
 
     if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength
+      isValid = value.length <= rules.maxLength && isValid
     }
 
     return isValid;
