a18a50229ee868907d0738605fd49d7cda698a36
Werstern <werstern@bigmir.net>
2019-02-27 23:43:00 +0200

    Logging user out

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -16,6 +16,7 @@ const authStart = (state, actios) => {
 };
 
 const authSuccess = (state, action) => {
+  console.log(action);
   return updateObject(state, {
     token: action.idToken,
     userId: action.userId,
@@ -31,11 +32,19 @@ const authFail = (state, action) => {
   });
 }
 
+const authLogout = (state, action) => {
+  return updateObject(state, {
+    token: null,
+    userId: null
+  });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START: return authStart(state, action);
     case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
     case actionTypes.AUTH_FAIL: return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
     default: return state;
   }
 }
